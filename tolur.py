71613efbb3def704591fe1f7bbd7f29af844881a
Kjartan Hrafnkelsson <kjartan@kvm.group>
2022-01-05 20:40:13 +0000

    Update tolur.py

--- tolur.py
+++ tolur.py
@@ -1,10 +1,5 @@
 def main():
-    name = input("Sláðu inn nafn > ")
-    i = len(name)
-    for x in range(len(name)):
-        name = name[-i:]
-        print(name)
-        i -= 1
+    # code
         
 if __name__ == '__main__':
     main()
